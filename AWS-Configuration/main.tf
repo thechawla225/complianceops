766517390dd938c9a36127acd732887e30195526
thechawla225 <ankitaryanchawla@gmail.com>
2026-09-15 15:48:56 +0530

    Added VPC configuration for 2 subnets in 2 regions for security and enabled NAT

--- AWS-Configuration/main.tf
+++ AWS-Configuration/main.tf
@@ -29,3 +29,18 @@ resource "aws_s3_bucket_public_access_block" "terraform-state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
+
+  name = "complianceops-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
