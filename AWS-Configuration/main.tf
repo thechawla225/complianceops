c7bb0b14b3a7769c82eeaa98732d9575cba3182e
thechawla225 <ankitaryanchawla@gmail.com>
2026-09-17 19:17:42 +0530

    commenting the non existent values for the workaround

--- AWS-Configuration/main.tf
+++ AWS-Configuration/main.tf
@@ -58,13 +58,13 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_groups = {
-    default = {
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
-    }
-  }
+  # eks_managed_node_groups = {
+  #   default = {
+  #     ami_type       = "AL2023_x86_64_STANDARD"
+  #     instance_types = ["t3.small"]
+  #     min_size       = 1
+  #     max_size       = 2
+  #     desired_size   = 1
+  #   }
+  # }
 }
