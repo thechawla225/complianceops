d0d45af4fec7c2284b7598901178045c291eefd4
thechawla225 <ankitaryanchawla@gmail.com>
2026-09-17 18:57:40 +0530

    Added EKS configuration

--- AWS-Configuration/main.tf
+++ AWS-Configuration/main.tf
@@ -44,3 +44,26 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
 }
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name = "complianceops-eks"
+
+  endpoint_public_access                   = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.small"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+    }
+  }
+}
